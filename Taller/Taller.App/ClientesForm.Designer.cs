00c4ded7a4576e317e19fafd873a678241bcdb93
EnriqueRTG <enriqueramontg@gmail.com>
2025-11-19 19:25:08 -0300

    Implementar CRUD completo de Clientes

---
+++ Taller/Taller.App/ClientesForm.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Taller.App
+{
+    partial class ClientesForm
+    {
+        /// <summary>
+        /// Contenedor de componentes generados por el diseñador.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpia los recursos que se estén utilizando.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        private void InitializeComponent()
+        {
+            this.panelSearch = new System.Windows.Forms.Panel();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.lblBuscar = new System.Windows.Forms.Label();
+            this.gridClientes = new System.Windows.Forms.DataGridView();
+            this.panelBottom = new System.Windows.Forms.Panel();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.lblPagina = new System.Windows.Forms.Label();
+            this.btnPaginaAnterior = new System.Windows.Forms.Button();
+            this.btnPaginaSiguiente = new System.Windows.Forms.Button();
+
+            this.panelSearch.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridClientes)).BeginInit();
+            this.panelBottom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelSearch
+            // 
+            this.panelSearch.Controls.Add(this.btnBuscar);
+            this.panelSearch.Controls.Add(this.txtBuscar);
+            this.panelSearch.Controls.Add(this.lblBuscar);
+            this.panelSearch.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelSearch.Location = new System.Drawing.Point(0, 0);
+            this.panelSearch.Name = "panelSearch";
+            this.panelSearch.Size = new System.Drawing.Size(800, 50);
+            this.panelSearch.TabIndex = 0;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnBuscar.Location = new System.Drawing.Point(702, 13);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBuscar.Location = new System.Drawing.Point(120, 14);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(576, 23);
+            this.txtBuscar.TabIndex = 1;
+            // 
+            // lblBuscar
+            // 
+            this.lblBuscar.AutoSize = true;
+            this.lblBuscar.Location = new System.Drawing.Point(12, 17);
+            this.lblBuscar.Name = "lblBuscar";
+            this.lblBuscar.Size = new System.Drawing.Size(102, 15);
+            this.lblBuscar.TabIndex = 0;
+            this.lblBuscar.Text = "Buscar (texto libre):";
+            // 
+            // gridClientes
+            // 
+            this.gridClientes.AllowUserToAddRows = false;
+            this.gridClientes.AllowUserToDeleteRows = false;
+            this.gridClientes.AllowUserToResizeRows = false;
+            this.gridClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridClientes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridClientes.Location = new System.Drawing.Point(0, 50);
+            this.gridClientes.MultiSelect = false;
+            this.gridClientes.Name = "gridClientes";
+            this.gridClientes.ReadOnly = true;
+            this.gridClientes.RowHeadersVisible = false;
+            this.gridClientes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridClientes.Size = new System.Drawing.Size(800, 360);
+            this.gridClientes.TabIndex = 1;
+            // 
+            // panelBottom
+            // 
+            this.panelBottom.Controls.Add(this.btnActualizar);
+            this.panelBottom.Controls.Add(this.btnEliminar);
+            this.panelBottom.Controls.Add(this.btnEditar);
+            this.panelBottom.Controls.Add(this.btnNuevo);
+            this.panelBottom.Controls.Add(this.lblPagina);
+            this.panelBottom.Controls.Add(this.btnPaginaAnterior);
+            this.panelBottom.Controls.Add(this.btnPaginaSiguiente);
+            this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelBottom.Location = new System.Drawing.Point(0, 410);
+            this.panelBottom.Name = "panelBottom";
+            this.panelBottom.Size = new System.Drawing.Size(800, 40);
+            this.panelBottom.TabIndex = 2;
+
+            // ...
+            // lblPagina
+            // 
+            this.lblPagina.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lblPagina.AutoSize = true;
+            this.lblPagina.Location = new System.Drawing.Point(270, 12);
+            this.lblPagina.Name = "lblPagina";
+            this.lblPagina.Size = new System.Drawing.Size(97, 15);
+            this.lblPagina.TabIndex = 4;
+            this.lblPagina.Text = "Página 1 de 1 (0)";
+            // 
+            // btnPaginaAnterior
+            // 
+            this.btnPaginaAnterior.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPaginaAnterior.Location = new System.Drawing.Point(550, 8);
+            this.btnPaginaAnterior.Name = "btnPaginaAnterior";
+            this.btnPaginaAnterior.Size = new System.Drawing.Size(75, 23);
+            this.btnPaginaAnterior.TabIndex = 5;
+            this.btnPaginaAnterior.Text = "< Anterior";
+            this.btnPaginaAnterior.UseVisualStyleBackColor = true;
+            this.btnPaginaAnterior.Click += new System.EventHandler(this.btnPaginaAnterior_Click);
+            // 
+            // btnPaginaSiguiente
+            // 
+            this.btnPaginaSiguiente.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPaginaSiguiente.Location = new System.Drawing.Point(631, 8);
+            this.btnPaginaSiguiente.Name = "btnPaginaSiguiente";
+            this.btnPaginaSiguiente.Size = new System.Drawing.Size(75, 23);
+            this.btnPaginaSiguiente.TabIndex = 6;
+            this.btnPaginaSiguiente.Text = "Siguiente >";
+            this.btnPaginaSiguiente.UseVisualStyleBackColor = true;
+            this.btnPaginaSiguiente.Click += new System.EventHandler(this.btnPaginaSiguiente_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnActualizar.Location = new System.Drawing.Point(713, 8);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnActualizar.TabIndex = 3;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(174, 8);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(93, 8);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 23);
+            this.btnEditar.TabIndex = 1;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(12, 8);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 23);
+            this.btnNuevo.TabIndex = 0;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+
+            // 
+            // ClientesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gridClientes);
+            this.Controls.Add(this.panelSearch);
+            this.Controls.Add(this.panelBottom);
+            this.Name = "ClientesForm";
+            this.Text = "Clientes";
+            this.panelSearch.ResumeLayout(false);
+            this.panelSearch.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridClientes)).EndInit();
+            this.panelBottom.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelSearch;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Label lblBuscar;
+        private System.Windows.Forms.DataGridView gridClientes;
+        private System.Windows.Forms.Panel panelBottom;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Label lblPagina;
+        private System.Windows.Forms.Button btnPaginaAnterior;
+        private System.Windows.Forms.Button btnPaginaSiguiente;
+
+    }
+}

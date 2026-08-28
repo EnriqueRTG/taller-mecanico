88156dbe677010f8d905fd37193a91c46d9e9198
Enrique Torres <enriqueramontg@gmail.com>
2026-08-28 09:17:38 -0300

    refactor: establecer arquitectura base del sistema

---
+++ Taller/Taller.Aplicacion/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Taller.Aplicacion;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddAplicacion(
+        this IServiceCollection services)
+    {
+        return services;
+    }
+}
---
+++ Taller/Taller.Infraestructura/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Taller.Infraestructura.Persistencia;
+
+namespace Taller.Infraestructura;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfraestructura(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString =
+            configuration.GetConnectionString("Default")
+            ?? throw new InvalidOperationException(
+                "No se encontró la cadena de conexión 'Default'.");
+
+        services.AddDbContext<TallerDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        return services;
+    }
+}

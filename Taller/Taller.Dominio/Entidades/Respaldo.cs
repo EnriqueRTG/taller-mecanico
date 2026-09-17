15921466c01864d4f52758fc9112647095c6932b
user-hakIA-grupo-10-at-760050153031 <gianemordacini@gmail.com>
2026-09-17 12:32:36 -0300

    formulario backup hecho, sin funcionalidades

---
+++ Taller/Taller.Dominio/Entidades/Respaldo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taller.Dominio.Entidades
+{
+    public class Respaldo
+    {
+        //public string NombreBaseDatos { get; set; }
+
+        //public string RutaArchivo { get; set; }
+
+        //public DateTime Fecha { get; set; }
+
+        //public long TamanoBytes { get; set; }
+    }
+}
---
+++ Taller/Taller.Presentacion/Formularios/Backup/Backup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Taller.Presentacion.Formularios.Backup
+{
+    public partial class Backup : Form
+    {
+        public Backup()
+        {
+            InitializeComponent();
+        }
+
+        private void RutaArchivo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
